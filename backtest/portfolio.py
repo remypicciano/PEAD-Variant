63a879e67a2af0043ca4c07f7c061efa04e3474e
remypicciano <245495112+remypicciano@users.noreply.github.com>
2026-08-26 19:45:40 -0400

    feat: daily position return and all daily close prices for ticker after entry date

--- backtest/portfolio.py
+++ backtest/portfolio.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from execution import Trade
 import pandas as pd 
 
 STARTING_CAPITAL = 100_000 
@@ -14,12 +15,15 @@ class Position:
     exit_price: float
     capital_allocated: float
 
-
+# Better to return a full dataframe with ticker and daily prices until the exit date
 class Portfolio: 
-    def __init__(self, starting_capital: float = STARTING_CAPITAL):
+    def __init__(self, trades: list[Trade], starting_capital: float = STARTING_CAPITAL):
         self.starting_capital = starting_capital
         self.cash = starting_capital
+        self.trades = trades
+
         self.positions = []
+        self.history = []
 
     def _get_trade_size(self) -> float:
         return self.cash * POSITION_SIZE
@@ -40,5 +44,21 @@ class Portfolio:
             capital_allocated=capital_allocated,
             dollar_return=dollar_return
         )
-
         self.positions.append(position) # don't return since we pend to self.positions
+
+        def _get_price(self, ticker: str, date: pd.Timestamp) -> float | None:
+            ticker_prices = self.prices[self.prices["ticker"].str.startswith(ticker)]
+            price = ticker_prices[ticker_prices["date"] == date]
+
+            if price.empty: 
+                return None
+
+            return float(price.iloc[0]["close"])
+
+        def _position_value(self, position: Position, current_price: float) -> float:
+            stock_return = (current_price-position.entry_price)/position.entry_price
+
+            if position.direction == "SHORT":
+                stock_return *= -1
+
+            return position.capital_allocated * (1 + stock_return) 
