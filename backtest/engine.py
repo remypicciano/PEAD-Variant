309b6293b608b6ba3b9028a06a83bf0727034afa
remypicciano <245495112+remypicciano@users.noreply.github.com>
2026-08-26 14:04:15 -0400

    test: print each individual signal

--- backtest/engine.py
+++ backtest/engine.py
@@ -1,12 +1,16 @@
+from data import MarketData, config
 from events import EventData
-from data import config, MarketData
+from strategy import Strategy
 
 data = MarketData(config)
 
 prices, earnings = data.load()
 
 event_data = EventData(prices, earnings)
-
 events = event_data.build()
 
-print(events)
+strategy = Strategy(prices, events)
+signals = strategy.build()
+
+for signal in signals:
+    print(signal)
